82fce46072137048a061e8ee9624741b3c787805
sohelranasakib <sohelrany802@gmail.com>
2024-03-09 02:49:07 +0600

    demo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Banner from "./components/banner/Banner"
+import Menu from "./components/menu/Menu"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+<Menu/>
+<Banner/>
+    </>
+  )
+}
+
+export default App
